936e126d7a6a0a10084673f785028b0d5cf7046d
Arjun Komath <arjunkomath@gmail.com>
2016-12-26 21:38:00 +0530

    Automate

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,28 @@
 
 var gulp = require('gulp');
 var bump = require('gulp-bump');
+var tag_version = require('gulp-tag-version');
+var git = require('gulp-git');
 
-gulp.task('bump', function(){
-  var options = {
-    type: 'minor'
-  };
-  gulp.src('./package.json')
-  .pipe(bump(options))
-  .pipe(gulp.dest('./'));
+gulp.task('bump', function () {
+	var options = {
+		type: 'minor'
+	};
+	gulp.src('./package.json')
+		.pipe(bump(options))
+		.pipe(gulp.dest('./'));
 });
 
-gulp.task('patch', function(){
-  gulp.src('./package.json')
-  .pipe(bump())
-  .pipe(gulp.dest('./build'));
+gulp.task('patch', function () {
+	gulp.src('./package.json')
+		.pipe(bump())
+		.pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['bump']);
+gulp.task('tag', function () {
+	return gulp.src(['./package.json'])
+		.pipe(git.commit('bumps package version'))
+		.pipe(tag_version());
+});
+
+gulp.task('default', ['bump', 'tag']);
