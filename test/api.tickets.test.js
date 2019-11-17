106de51ff27e3dc98f62e1357fd871877803da64
Arjun Komath <arjunkomath@gmail.com>
2019-11-17 17:17:07 +1100

    Add tests for list tickets

--- test/api.tickets.test.js
+++ test/api.tickets.test.js
@@ -22,7 +22,7 @@ const nock = require('nock')
 
 const Freshdesk = require('..')
 
-describe('api.tickets', function(){
+describe('api.tickets', function () {
 
 	const freshdesk = new Freshdesk('https://test.freshdesk.com', 'TESTKEY')
 
@@ -35,15 +35,15 @@ describe('api.tickets', function(){
 			beforeEach(() => {
 				res = [
 					{
-						"id" : 1,
-						"description" : "Ticket requester",
-						"label" : "Search a requester",
-						"name" : "requester",
-						"position" : 1,
-						"portal_cc" : false,
-						"portal_cc_to" : "company",
-						"type" : "default_requester"
-					}
+						"id": 1,
+						"description": "Ticket requester",
+						"label": "Search a requester",
+						"name": "requester",
+						"position": 1,
+						"portal_cc": false,
+						"portal_cc_to": "company",
+						"type": "default_requester",
+					},
 				]
 
 				// SET UP expected request
@@ -53,7 +53,7 @@ describe('api.tickets', function(){
 					.reply(200, res)
 			})
 
-			it('should send GET request to /api/v2/ticket_fields', function(done) {
+			it('should send GET request to /api/v2/ticket_fields', function (done) {
 				freshdesk.listAllTicketFields((err, data) => {
 					expect(err).is.null
 					expect(data).to.deep.equal(res)
@@ -62,7 +62,7 @@ describe('api.tickets', function(){
 				})
 			})
 
-			it('(explicit null) should send GET request to /api/v2/ticket_fields', function(done) {
+			it('(explicit null) should send GET request to /api/v2/ticket_fields', function (done) {
 				freshdesk.listAllTicketFields(null, (err, data, extra) => {
 					expect(err).is.null
 					expect(data).to.deep.equal(res)
@@ -80,14 +80,14 @@ describe('api.tickets', function(){
 
 			beforeEach(() => {
 				res = [{
-					"id" : 1,
-					"description" : "Ticket requester",
-					"label" : "Search a requester",
-					"name" : "requester",
-					"position" : 1,
-					"portal_cc" : false,
-					"portal_cc_to" : "company",
-					"type" : "default_requester"
+					"id": 1,
+					"description": "Ticket requester",
+					"label": "Search a requester",
+					"name": "requester",
+					"position": 1,
+					"portal_cc": false,
+					"portal_cc_to": "company",
+					"type": "default_requester",
 				}]
 
 				// SET UP expected request
@@ -97,7 +97,7 @@ describe('api.tickets', function(){
 					.reply(200, res)
 			})
 
-			it('should send GET request to /api/v2/ticket_fields?type=xx', function(done) {
+			it('should send GET request to /api/v2/ticket_fields?type=xx', function (done) {
 
 				const params = {'type': 'default_requester'}
 
@@ -111,5 +111,114 @@ describe('api.tickets', function(){
 
 		})
 
+	})
+
+	describe('listAllTicket', () => {
+
+		describe('without params', () => {
+
+			let res = null
+			let params = {}
+
+			beforeEach(() => {
+				res = [{
+					"cc_emails": ["user@cc.com", "user2@cc.com"],
+					"fwd_emails": [],
+					"reply_cc_emails": ["user@cc.com", "user2@cc.com"],
+					"fr_escalated": false,
+					"spam": false,
+					"email_config_id": null,
+					"group_id": 2,
+					"priority": 1,
+					"requester_id": 5,
+					"responder_id": 1,
+					"source": 2,
+					"status": 2,
+					"subject": "Please help",
+					"to_emails": null,
+					"product_id": null,
+					"id": 18,
+					"type": "Lead",
+					"created_at": "2015-08-17T12:02:50Z",
+					"updated_at": "2015-08-17T12:02:51Z",
+					"due_by": "2015-08-20T11:30:00Z",
+					"fr_due_by": "2015-08-18T11:30:00Z",
+					"is_escalated": false,
+					"custom_fields": {
+						"category": "Default",
+					},
+				}]
+
+				// SET UP expected request
+				nock('https://test.freshdesk.com')
+					.get('/api/v2/tickets')
+					.query(params)
+					.reply(200, res)
+			})
+
+			it('should send GET request to /api/v2/tickets', function (done) {
+
+				freshdesk.listAllTickets(params, (err, data) => {
+					expect(err).is.null
+					expect(data).to.deep.equal(res)
+
+					done()
+				})
+			})
+
+		})
+
+		describe('with params', () => {
+
+			let res = null
+			let params = {'type': 'default_requester'}
+
+			beforeEach(() => {
+				res = [{
+					"cc_emails": ["user@cc.com", "user2@cc.com"],
+					"fwd_emails": [],
+					"reply_cc_emails": ["user@cc.com", "user2@cc.com"],
+					"fr_escalated": false,
+					"spam": false,
+					"email_config_id": null,
+					"group_id": 2,
+					"priority": 1,
+					"requester_id": 5,
+					"responder_id": 1,
+					"source": 2,
+					"status": 2,
+					"subject": "Please help",
+					"to_emails": null,
+					"product_id": null,
+					"id": 18,
+					"type": "Lead",
+					"created_at": "2015-08-17T12:02:50Z",
+					"updated_at": "2015-08-17T12:02:51Z",
+					"due_by": "2015-08-20T11:30:00Z",
+					"fr_due_by": "2015-08-18T11:30:00Z",
+					"is_escalated": false,
+					"custom_fields": {
+						"category": "Default",
+					},
+				}]
+
+				// SET UP expected request
+				nock('https://test.freshdesk.com')
+					.get('/api/v2/tickets')
+					.query(params)
+					.reply(200, res)
+			})
+
+			it('should send GET request to /api/v2/tickets', function (done) {
+
+				freshdesk.listAllTickets(params, (err, data) => {
+					expect(err).is.null
+					expect(data).to.deep.equal(res)
+
+					done()
+				})
+			})
+
+		})
 	})
 })
